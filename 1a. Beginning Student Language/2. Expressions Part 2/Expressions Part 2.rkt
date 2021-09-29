e6d95fb78380e38e402358715f93c77e777675a6
Xpertoforever <91433157+Xpertoforever@users.noreply.github.com>
2021-09-29 11:56:02 +0300

    Add files via upload

---
+++ 1a. Beginning Student Language/2. Expressions Part 2/Expressions Part 2.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |Expressions Part 2|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;Solution of the previous Problem
+;(sqrt (+ (sqr 3) (sqr 4)))
+
+;Irracional number
+(sqrt 2)
+; The answer #i1.414213....
+; The #i means that this number is pretty close, but not exactly
