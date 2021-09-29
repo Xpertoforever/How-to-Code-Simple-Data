906f200fdf41e567a158af67021007244c617091
Xpertoforever <91433157+Xpertoforever@users.noreply.github.com>
2021-09-29 05:29:20 +0300

    Add files via upload

---
+++ 1a. Beginning Student Language/1. Expressions Part 1/1. Expressions Part 1.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |1. Expressions pt 1|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;To form an expression
+;<Value>
+;(<primitive> <expression>...)
+;Semicolon to comment
+
+;(+ 3 4)
+;(+ 3 (* 2 3))
+;(/ 12 (* 2 3))
+
+(sqr 3)
+(sqr 16)
+
